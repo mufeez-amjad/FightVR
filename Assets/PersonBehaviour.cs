389a48c2019458ed7529ad608d4053665ae0e495
mufeez-amjad <zeefumdajma@outlook.com>
2018-07-27 23:25:00 -0400

    Initial files

---
+++ Assets/PersonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W)){
+
+            gameObject.transform.Translate(0f, 1f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            gameObject.transform.Translate(0f, -1f, 0f);
+        }
+    }
+}
