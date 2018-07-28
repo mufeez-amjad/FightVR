86516adc464bd6fac702ad36c4fc7f0fcd773171
Ethan Guo <ethguo@gmail.com>
2018-07-28 04:02:19 -0400

    Body following and movement

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.eulerAngles = new Vector3(0f, mainCamera.transform.eulerAngles.y, 0f);
+
+		if (Input.GetKey(KeyCode.W) || Input.GetMouseButton(0)){
+            float facing = mainCamera.transform.eulerAngles.y;
+
+            gameObject.transform.Translate(0f, 0f, 0.1f);
+        }
+    }
+}
