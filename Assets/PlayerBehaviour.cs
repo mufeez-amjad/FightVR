e0b7231ceff65a71e65048ca556028f86677963d
Curtis Chong <curtischong5@gmail.com>
2018-07-28 23:17:21 -0400

    made health flash

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerBehaviour : MonoBehaviour {
+    public Image healthFilter;
+
     Camera mainCamera;
 
+
+    private int maxHealth = 100;
+    private int health = 100;
 	// Use this for initialization
 	void Start () {
         mainCamera = Camera.main;
@@ -22,8 +29,46 @@ public class PlayerBehaviour : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.A)){ // TEMPORARY: made teleport   
             gameObject.transform.position += gameObject.transform.forward * 10;
         }
+        if (Input.GetKeyDown(KeyCode.J)){ // TEMPORARY: made damage self   
+            takeDamage(20);
+        }
     }
     void OnCollisionEnter(Collision col){
         Debug.Log("Collided with player");
+    }
+
+    private YieldInstruction fadeInstruction = new YieldInstruction();
+    float fadeTime = 2.0f;
+    IEnumerator FadeOut(Image image){
+        float elapsedTime = 0.0f;
+        Color c = image.color;
+        while (elapsedTime < fadeTime){
+            yield return fadeInstruction;
+            elapsedTime += Time.deltaTime;
+            c.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeTime);
+            image.color = c;
+        }
+    }
+
+
+    private void checkFilter(){
+        Debug.Log(health);
+        if(health <= 10){
+           healthFilter.color = new Color(255, 0, 0, 0.9f);
+        }else if(health <= 20){
+           healthFilter.color = new Color(255, 0, 0, 0.6f);
+        }else if(health <= 40){
+           healthFilter.color = new Color(255, 0, 0, 0.35f);
+        }else if(health <= 60){
+           healthFilter.color = new Color(255, 0, 0, 0.1f);
+        }else if(health <= 80){
+           healthFilter.color = new Color(255, 0, 0, 0.05f);
+        }
+        StartCoroutine(FadeOut(healthFilter));
+    }
+
+    public void takeDamage(int amount){
+        health -=amount;
+        checkFilter();
     }
 }
