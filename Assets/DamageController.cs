131d1380cf154a8854146e3bdaaa9faff83b6686
Ethan Guo <ethguo@gmail.com>
2018-07-29 13:42:25 -0400

    Added Velocity-based DamageController (Curtis)

---
+++ Assets/DamageController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour {
+
+	private GameObject sword;
+	// Update is called once per frame
+	void Update () {
+		sword = GameObject.FindGameObjectWithTag("Sword");
+		float speed = sword.GetComponent<Rigidbody>().velocity.magnitude;
+
+
+		if (speed > 2){
+			GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+			foreach (GameObject g in enemies){
+				//g.die();
+				Destroy(g);
+			}
+		}
+	}
+}
